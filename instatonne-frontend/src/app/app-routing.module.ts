3b8d8e175525472b535f074ff8b98ab09a05b317
Felix Wortmann <felixwortmann@protonmail.com>
2020-04-20 16:24:54 +0200

    create models from here: https://github.com/felixwortmann/Instatonne/tree/master/backend/src/main/kotlin/de/instatonne/backend/models

--- instatonne-frontend/src/app/app-routing.module.ts
+++ instatonne-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ImageComponent} from './components/image/image.component';
+import {PostComponent} from './components/post/post.component';
+import {ProfileComponent} from './components/profile/profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'image', component: ImageComponent},
+  {path: 'post', component: PostComponent},
+  {path: 'profile', component: ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
