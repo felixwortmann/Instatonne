d8aaa3af94da327872afcd8db4b4b6678eec6d36
Felix Wortmann <felixwortmann@protonmail.com>
2020-04-20 16:24:08 +0200

    added basic components

---
+++ instatonne-frontend/src/app/components/post-header/post-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-header',
+  templateUrl: './post-header.component.html',
+  styleUrls: ['./post-header.component.scss']
+})
+export class PostHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ instatonne-frontend/src/app/components/profile-header/profile-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-header',
+  templateUrl: './profile-header.component.html',
+  styleUrls: ['./profile-header.component.scss']
+})
+export class ProfileHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
