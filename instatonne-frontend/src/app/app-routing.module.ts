d5866de2394978f893aac3c8dae016b9b131adf5
Florian Redmann <Florian.Redmann@rewe-group.com>
2020-04-20 16:57:56 +0200

    #3 | Create post
    - Added basic functionality to add an image

--- instatonne-frontend/src/app/app-routing.module.ts
+++ instatonne-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'create', component: CreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- instatonne-frontend/src/app/app.module.ts
+++ instatonne-frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './components/post/post.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateComponent } from './components/create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
