11c355a68387bd0a7e14bde11004aaf43725c865
Felix Wortmann <felixwortmann@protonmail.com>
2020-04-20 16:25:53 +0200

    updated .gitignore

--- instatonne-frontend/src/app/app.module.ts
+++ instatonne-frontend/src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponent } from './components/post/post.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ImageComponent } from './components/image/image.component';
+import { PostHeaderComponent } from './components/post-header/post-header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import { ProfileComponent } from './components/profile/profile.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { ProfileHeaderComponent } from './components/profile-header/profile-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    ImageComponent,
+    PostHeaderComponent,
+    ProfileComponent,
+    ProfileHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
