30b6476fabf4a241d8e2cbaa2a27d860d65c50b3
timgrohmann <timgrohmann@icloud.com>
2020-04-27 22:06:42 +0200

    globalized login and made use of it in ProfileHeader

--- instatonne-frontend/src/app/components/login/login.component.ts
+++ instatonne-frontend/src/app/components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, AfterViewInit, ChangeDetectorRef, ApplicationRef, Ng
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Observable, of, Subject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { User } from 'src/app/generated/models';
 
@@ -19,47 +19,19 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private ngZone: NgZone,
     private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.loadGAPI();
-    window['onSignIn'] = this.onSignIn.bind(this);
-
     this.user$ = this.authService.getUser();
     this.authUser$ = this.authService.getAuthUser();
-  }
 
-  public onSignIn(googleUser: gapi.auth2.GoogleUser) {
-    const profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-    console.log(googleUser.getAuthResponse().id_token);
-    this.ngZone.run(() => {
-      this.authService.setAuthUser(googleUser);
+    this.user$.pipe(filter(user => !!user)).subscribe(_ => {
+      this.router.navigate(['profile']);
     });
   }
 
-  private loadGAPI() {
-    let isFound = false;
-    const scripts = Array.from(document.getElementsByTagName('script'));
-    for (const script of scripts) {
-      if (script.getAttribute('src') != null && script.getAttribute('src').includes('https://apis.google.com/js/platform.js')) {
-        isFound = true;
-      }
-    }
 
-    if (!isFound) {
-      const node = document.createElement('script');
-      node.src = 'https://apis.google.com/js/platform.js';
-      node.type = 'text/javascript';
-      node.async = false;
-      document.getElementsByTagName('head')[0].appendChild(node);
-    }
-  }
 
   onRegister(f: NgForm) {
     if (!f.valid) { return; }
--- instatonne-frontend/src/app/services/auth.service.ts
+++ instatonne-frontend/src/app/services/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Subject, Observable, BehaviorSubject, ReplaySubject, of, empty, EMPTY, combineLatest, throwError } from 'rxjs';
 import { User } from '../generated/models';
-import { switchMap, mapTo, map, tap, catchError, mergeMap } from 'rxjs/operators';
+import { switchMap, mapTo, map, tap, catchError, mergeMap, share } from 'rxjs/operators';
 import { UsersService } from '../generated/services';
 import { HttpErrorResponse } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,16 @@ export class AuthService {
   private username = new Subject<string>();
 
   constructor(
-    private userService: UsersService
-  ) { }
+    private userService: UsersService,
+    private cookieService: CookieService,
+    private ngZone: NgZone
+  ) {
+    this.loadGAPI();
+    window['onSignIn'] = this.onSignIn.bind(this);
+    window['gapiInit'] = this.onAPILoad.bind(this);
+
+    // this.cookieService.get
+  }
 
   setAuthUser(user: gapi.auth2.GoogleUser) {
     this.authUser.next(user);
@@ -45,9 +54,11 @@ export class AuthService {
             // if the user does not yet exist, we are fine with it and ask the user to register
             return EMPTY;
           }
+          console.log('a bad error :( ', err);
           throwError(err);
         }));
-      })
+      }),
+      share()
     );
   }
 
@@ -61,6 +72,52 @@ export class AuthService {
       console.log('attempting to reload user');
       this.reloadUser.next(null);
     });
+  }
+
+  public onSignIn(googleUser: gapi.auth2.GoogleUser) {
+    const profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    console.log(googleUser.getAuthResponse().id_token);
+    this.ngZone.run(() => {
+      this.setAuthUser(googleUser);
+    });
+  }
+
+  public onAPILoad() {
+    gapi.load('auth2', () => {
+      gapi.auth2.init({}).then(_ => {
+        const signedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+        if (signedIn) {
+          this.ngZone.run(() => {
+            this.setAuthUser(gapi.auth2.getAuthInstance().currentUser.get());
+          });
+        }
+        console.log('signed in', gapi.auth2.getAuthInstance().isSignedIn.get());
+      });
+      /*gapi.auth2.getAuthInstance().signIn();
+      console.log('auth api inited');*/
+    });
+  }
+
+  private loadGAPI() {
+    let isFound = false;
+    const scripts = Array.from(document.getElementsByTagName('script'));
+    for (const script of scripts) {
+      if (script.getAttribute('src') != null && script.getAttribute('src').includes('https://apis.google.com/js/platform.js')) {
+        isFound = true;
+      }
+    }
+
+    if (!isFound) {
+      const node = document.createElement('script');
+      node.src = 'https://apis.google.com/js/platform.js?onload=gapiInit';
+      node.type = 'text/javascript';
+      node.async = false;
+      document.getElementsByTagName('head')[0].appendChild(node);
+    }
   }
 
 }
