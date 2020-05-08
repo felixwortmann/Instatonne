fdd0aca7590f8297dc4661bed21db849fae1a22e
timgrohmann <timgrohmann@icloud.com>
2020-05-08 17:28:47 +0200

    externalized ws config

--- instatonne-frontend/src/environments/environment.prod.ts
+++ instatonne-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://it-api.timgrohmann.de'
+  apiUrl: 'http://it-api.timgrohmann.de',
+  websocketEndpoint: 'ws://it-api.timgrohmann.de/ws-stomp'
 };
--- instatonne-frontend/src/environments/environment.ts
+++ instatonne-frontend/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080',
+  websocketEndpoint: 'ws://localhost:8080/ws-stomp'
 };
 
 /*
