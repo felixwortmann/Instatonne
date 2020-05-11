916484f4b59e0be4d44416ae28a3f3472ad657bd
timgrohmann <timgrohmann@icloud.com>
2020-05-11 09:42:13 +0200

    added certificates and api uses ssl

--- instatonne-frontend/src/environments/environment.prod.ts
+++ instatonne-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://it-api.timgrohmann.de',
+  apiUrl: 'https://it-api.timgrohmann.de',
   websocketEndpoint: 'ws://it-api.timgrohmann.de/ws-stomp'
 };
