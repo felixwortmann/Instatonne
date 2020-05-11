35d6ec7f8263444e25bd96e73f5fd20bfad570c3
elsholz <hendrik.lankers.hl@googlemail.com>
2020-05-11 16:16:03 +0200

    add comment-section to routing as `comment` for testing purposes

--- instatonne-frontend/src/app/app-routing.module.ts
+++ instatonne-frontend/src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 import { ProfileSubscriptionsComponent } from './components/profile/profile-subscriptions/profile-subscriptions.component';
 import { ProfileEditComponent } from './components/profile/profile-edit/profile-edit.component';
 import { MessagingComponent } from './components/messaging/messaging.component';
+import {CommentSectionComponent} from "./components/comment-section/comment-section.component";
 
 
 const routes: Routes = [
@@ -19,6 +20,7 @@ const routes: Routes = [
   { path: 'u/:username/f', component: ProfileSubscriptionsComponent },
   { path: 'editProfile', component: ProfileEditComponent },
   { path: 'm/:username', component: MessagingComponent },
+  { path: 'comment', component: CommentSectionComponent }
 ];
 
 @NgModule({
