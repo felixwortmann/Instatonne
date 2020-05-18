759ba9fca8f93f6adb08882476456f3a19c7f38b
Felix Wortmann <felixwortmann@protonmail.com>
2020-05-18 14:47:45 +0200

    removed logging

--- instatonne-frontend/src/app/app-routing.module.ts
+++ instatonne-frontend/src/app/app-routing.module.ts
@@ -1,26 +1,26 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {HttpClientModule} from '@angular/common/http';
-import {ImageComponent} from './components/image/image.component';
-import {CreateComponent} from './components/create/create.component';
-import {ProfileComponent} from './components/profile/profile.component';
-import {LoginComponent} from './components/login/login.component';
-import {ProfileSubscriptionsComponent} from './components/profile/profile-subscriptions/profile-subscriptions.component';
-import {ProfileEditComponent} from './components/profile/profile-edit/profile-edit.component';
-import {MessagingComponent} from './components/messaging/messaging.component';
-import {TimelineComponent} from './components/timeline/timeline.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ImageComponent } from './components/image/image.component';
+import { CreateComponent } from './components/create/create.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileSubscriptionsComponent } from './components/profile/profile-subscriptions/profile-subscriptions.component';
+import { ProfileEditComponent } from './components/profile/profile-edit/profile-edit.component';
+import { MessagingComponent } from './components/messaging/messaging.component';
+import { TimelineComponent } from './components/timeline/timeline.component';
 
 
 const routes: Routes = [
-  {path: 'image', component: ImageComponent},
-  {path: 'create', component: CreateComponent},
-  {path: 'u', component: ProfileComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'u/:username', component: ProfileComponent},
-  {path: 'u/:username/f', component: ProfileSubscriptionsComponent},
-  {path: 'editProfile', component: ProfileEditComponent},
-  {path: 'm/:username', component: MessagingComponent},
-  {path: 'timeline', component: TimelineComponent}
+  { path: 'image', component: ImageComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'u', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'u/:username', component: ProfileComponent },
+  { path: 'u/:username/f', component: ProfileSubscriptionsComponent },
+  { path: 'editProfile', component: ProfileEditComponent },
+  { path: 'm/:username', component: MessagingComponent },
+  { path: 'timeline', component: TimelineComponent }
 ];
 
 @NgModule({
